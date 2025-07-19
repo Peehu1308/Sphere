087895149d576f092b214444656412c9f69d4639
Peehu Mishra <peehumishra1308@gmail.com>
2025-07-20 02:44:15 +0530

    Update navbar.dart

--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:sphere/Screens/BlogScreen.dart';
 import 'package:sphere/Screens/MoodScreen.dart';
 import 'package:sphere/Screens/MusicScreen.dart';
+
 class Navbar extends StatefulWidget {
   final int currentIndex;
   final Function(int) onTap;
-  const Navbar({super.key,
-    required this.currentIndex,
-    required this.onTap,
-  });
+  final String token;
+
+  const Navbar({super.key, required this.currentIndex, required this.onTap,required this.token});
 
   @override
   State<Navbar> createState() => _NavbarState();
@@ -19,28 +19,43 @@ class _NavbarState extends State<Navbar> {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       currentIndex: widget.currentIndex,
-      onTap: (index){
-        if(index==0){
-          Navigator.push(context,MaterialPageRoute(builder: (context)=> const MusicScreen()));
-        }
-        else if(index==1){
-          Navigator.push(context,MaterialPageRoute(builder: (context)=> const MoodScreen()));
-        }
-        else if(index==2){
-          Navigator.push(context,MaterialPageRoute(builder: (context)=> const BlogScreen()));
+      onTap: (index) {
+        if (index == 0) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => MusicScreen(token:widget.token,)),
+          );
+        } else if (index == 1) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MoodScreen(token:widget.token),
+            ),
+          );
+        } else if (index == 2) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => BlogScreen(token: widget.token,)),
+          );
         }
       },
       backgroundColor: Colors.white,
-      
+
       selectedItemColor: Colors.black,
       unselectedItemColor: Colors.grey,
       showUnselectedLabels: true,
       type: BottomNavigationBarType.fixed,
       items: [
-      BottomNavigationBarItem(icon: Icon(Icons.music_note,),
-      label: 'Music'),
-      BottomNavigationBarItem(icon: Icon(Icons.border_all_rounded,),label: 'Mood'),
-      BottomNavigationBarItem(icon: Icon(Icons.article_outlined,),label: 'Blog'),
-    ]);
+        BottomNavigationBarItem(icon: Icon(Icons.music_note), label: 'Music'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.border_all_rounded),
+          label: 'Mood',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.article_outlined),
+          label: 'Blog',
+        ),
+      ],
+    );
   }
 }
