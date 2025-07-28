c442e0cdced0a127adbca3a93ec9bb91d5e83d94
Peehu Mishra <peehumishra1308@gmail.com>
2025-07-28 21:56:39 +0530

    Update MoodScreen.dart

--- lib/Screens/MoodScreen.dart
+++ lib/Screens/MoodScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sphere/Screens/Canvas.dart';
 // import 'package:sphere/components/MoodScreen_box.dart';
 import 'package:sphere/components/mood_board.dart';
 import 'package:sphere/components/navbar.dart';
@@ -6,7 +8,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class MoodScreen extends StatefulWidget {
   final String token;
-  const MoodScreen({super.key,required this.token});
+  const MoodScreen({super.key, required this.token});
 
   @override
   State<MoodScreen> createState() => _MoodScreenState();
@@ -17,10 +19,36 @@ class _MoodScreenState extends State<MoodScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      
-      
+
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Sphere",
+              style: GoogleFonts.pacifico(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              child: Icon(Icons.create),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyDrawingApp()),
+                );
+              },
+            ),
+          ],
+        ),
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: Colors.black,
+      ),
       body: Padding(
-        padding: const EdgeInsets.only(top:10.0),
+        padding: const EdgeInsets.only(top: 10.0),
         child: SingleChildScrollView(
           child: Center(
             child: Column(
@@ -35,7 +63,11 @@ class _MoodScreenState extends State<MoodScreen> {
           ),
         ),
       ),
-      bottomNavigationBar: Navbar(currentIndex: 1, onTap: (index) {}, token:widget.token,),
+      bottomNavigationBar: Navbar(
+        currentIndex: 1,
+        onTap: (index) {},
+        token: widget.token,
+      ),
     );
   }
 }
